8dfce25d03e5cd798c2e5e69f005e011d373029f
mjadach <m.jadach96@gmail.com>
2017-03-19 23:50:45 +0100

    komit z brancza

--- Lab_1/Problem_1/main.py
+++ Lab_1/Problem_1/main.py
@@ -33,5 +33,5 @@ if __name__ == '__main__':
     list_output = find_in_files(files_list, string_to_look_for)
     list_to_file(file_name, list_output)
     os.system("jupyter notebook ./" + file_name)
-
+#komentarz z branczca
     #jak zobrazować wyjście??
