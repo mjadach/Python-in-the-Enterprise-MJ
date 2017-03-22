ee0a8fdf4531e51cb22f5baa973bf990d3bcae31
mjadach <m.jadach96@gmail.com>
2017-03-22 14:36:08 +0100

    Calculator patch

--- Lab_2/Problem_1_Calculator_v2/Main.py
+++ Lab_2/Problem_1_Calculator_v2/Main.py
@@ -2,3 +2,5 @@ from Lab_2.Problem_1_Calculator_v2.Calculator import Calculator
 
 myCalculator = Calculator()
 print(myCalculator.Divide(4,2))
+print(myCalculator.Add(4,2))
+print(myCalculator.Derivate("3*x",4)) # this one returns wrong value
